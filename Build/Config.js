3eb33e4f25f59659c9ed2fae523e6552a4e190f3
Andrey1024 <vlasov-andrey@mail.ru>
2016-05-06 20:11:46 +0300

    Built solver from commit f00cdaf on branch newInterface

--- Build/Config.js
+++ Build/Config.js
@@ -1,7 +1,7 @@
 require.config({
     paths: {
         'solver': '../lib/solver/js-solver',
-        'idd': '../lib/IDD/idd',
+        'idd': '../lib/idd/idd',
         'jquery': '../lib/jquery/jquery',
         'modernizer': "../lib/modernizr/modernizr",
         "rx": "../lib/rxjs/rx.all",
